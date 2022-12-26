c6cf869a69487df5a387b8d1807a8ab06c0c98eb
csk200387 <102679480+csk200387@users.noreply.github.com>
2022-12-26 19:59:19 +0900

    1226 1.12 update

--- ModsAutoPatcher/autoPatcher.py
+++ ModsAutoPatcher/autoPatcher.py
@@ -5,6 +5,7 @@ from time import sleep, strftime
 
 username = getuser()
 path = f"C:\\Users\\{username}\\AppData\\Roaming\\.minecraft\\mods" # REAL PATH
+forgePath = f"C:\\Users\\{username}\\Desktop"
 #path = "C:\\Users\\csk20\\Desktop\\pyinstaller\\mods" # TEST PATH
 
 def openModsFolder() :
@@ -37,7 +38,7 @@ def checkMods() :
             for i in list :
                 move(i, fname)
         except :
-            print(f"{fname} 과 같은 이름의 폴더가 있습니다\n폴더를 삭제하고 다시 실행해주세요")
+            print(f"{fname} 과 같은 이름의 폴더가 있습니다\n폴더를 삭제하고 다시 실행해주세요.")
             os.startfile(path)
             os.system("pause")
             exit()
@@ -52,10 +53,31 @@ def fileDownload() :
         down = requests.get(i)
         with open(fileName, "wb") as f :
             f.write(down.content)
+    print(f"모드 {len(list)} 개를 모두 적용하였습니다.")
+
+def forgeDownload() :
+    os.chdir(forgePath)
+    try :
+        os.mkdir("ForgeInstaller")
+    except :
+        pass
+    os.chdir("ForgeInstaller")
+    req = requests.get("https://raw.githubusercontent.com/csk200387/Toy/main/ModsAutoPatcher/forgeVersion.txt")
+    data = req.text.strip()
+    fileName = data.split("/")[-1]
+    print("바탕화면에",fileName,"을 다운받습니다.")
+    print(f"Download : {fileName}") 
+    down = requests.get(data)
+    with open(fileName, "wb") as f :
+        f.write(down.content)
+    print("forge 설치가 끝난 후 키를 눌러주세요.")
+    os.system("java -jar "+fileName)
+    os.system("pause")
+    print("바탕화면에 ForgeInstaller 폴더는 지우시면 됩니다.")
 
 if __name__ == "__main__" :
     os.chdir(path)
-    menu = input("마인크래프트 모드 반자동 적용기 1.0v\n메뉴 :\n1. mods파일 열기\n2. 모드 자동패치\n3. 프로그램 종료 : ")
+    menu = input("마인크래프트 모드 반자동 적용기 1.2v\n메뉴 :\n1. mods파일 열기\n2. 모드 자동패치\n3. Forge 설치\n4. 프로그램 종료 : ")
     if menu == "1" :
         openModsFolder()
     elif menu == "2" :
@@ -64,5 +86,7 @@ if __name__ == "__main__" :
         fileDownload()
         os.startfile(path)
     elif menu == "3" :
+        forgeDownload()
+    elif menu == "4" :
         exit()
     os.system("pause")
